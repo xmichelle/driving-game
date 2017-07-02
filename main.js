287770d38f36627ffc4239f2761cb61dcd338866
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-02 14:31:37 -0700

    Created DOM element for car image and appended it to the document body. #6

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
 
-const $canvas = document.querySelector('canvas')
-const $c = $canvas.getContext('2d')
+const $car = document.createElement('img')
+$car.setAttribute('src', 'images/car.png')
 
-$canvas.width = window.innerWidth
-$canvas.height = window.innerHeight
-
-function drawCar() {
-  $c.fillRect(30, 30, 50, 30)
-
-  $c.beginPath()
-  $c.arc(40, 60, 7, 0, Math.PI, false)
-  $c.closePath()
-  $c.fillStyle = '#8B4513'
-  $c.fill()
-
-  $c.beginPath()
-  $c.arc(70, 60, 7, 0, Math.PI, false)
-  $c.closePath()
-  $c.fillStyle = '#8B4513'
-  $c.fill()
-
-  $c.beginPath()
-  $c.arc(40, 30, 7, 0, Math.PI, true)
-  $c.closePath()
-  $c.fillStyle = '#8B4513'
-  $c.fill()
-
-  $c.beginPath()
-  $c.arc(70, 30, 7, 0, Math.PI, true)
-  $c.closePath()
-  $c.fillStyle = '#8B4513'
-  $c.fill()
-}
-
-drawCar()
+document.body.appendChild($car)

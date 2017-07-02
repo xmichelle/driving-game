2e76f9958021731b0b026a84eb95ccd415548251
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-02 14:56:17 -0700

    Created a class function for the movement of the car. #6

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
 
-const $car = document.createElement('img')
-$car.setAttribute('src', 'images/car.png')
-$car.classList.add('car')
+const $carImage = document.createElement('img')
+$carImage.setAttribute('src', 'images/car.png')
+$carImage.classList.add('car')
 
-document.body.appendChild($car)
+document.body.appendChild($carImage)
+
+class Car {
+  constructor(direction, speed, location) {
+    this.direction = direction
+    this.speed = speed
+    this.location = location
+  }
+
+  turn(direction) {
+    this.direction = direction
+  }
+
+  accelerate(amount) {
+    this.speed += amount
+  }
+
+  move() {
+    switch (this.direction) {
+      case 'north':
+        this.location[1] -= this.speed
+        break
+      case 'south':
+        this.location[1] += this.speed
+        break
+      case 'east':
+        this.location[0] += this.speed
+        break
+      case 'west':
+        this.location[0] -= this.speed
+    }
+  }
+
+  static start(car) {
+    setInterval(function () {
+      car.move()
+    }, 1000)
+  }
+}
+
+const car = new Car('south', 5, [5, 5])
+
+Car.start(car)
